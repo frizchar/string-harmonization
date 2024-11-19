2c14ce03a56a0f75e4c194e749a02cdb773bcfed
frizchar <cchrysanth@gmail.com>
2024-11-19 11:33:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ list_B = ['string1a', 'string2', 'string1rr', 'string3b_23', 'string3.']
 
 results_list = []
 
-for item_b in list_B:
+for item_a in list_A:
     max_distance = 0
     closest_match = None
-    for item_a in list_A:
+    for item_b in list_B:
         distance = fuzz.ratio(item_b, item_a)
         if distance > max_distance:
             max_distance = distance
-            closest_match = item_a
-    results_list.append([item_b, closest_match, max_distance])
+            closest_match = item_b
+    results_list.append([item_a, closest_match, max_distance])
 
 print('results:', results_list)
