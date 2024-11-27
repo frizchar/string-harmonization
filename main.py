d0ca9ae2558e1e3504208b56154308aebc16e0f7
frizchar <cchrysanth@gmail.com>
2024-11-27 10:35:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 from fuzzywuzzy import fuzz
 
+
+def find_closest_matches(set_A, set_B):
+    results_list = []
+
+    for item_a in set_A:
+        max_distance = 0
+        closest_match = None
+        for item_b in set_B:
+            distance = fuzz.ratio(item_b, item_a)
+            if distance > max_distance:
+                max_distance = distance
+                closest_match = item_b
+        results_list.append([item_a, closest_match, max_distance])
+
+    return results_list
+
+
+# Example usage
 set_A = {'string1', 'string2', 'string3'}
 set_B = {'string1a', 'string2', 'string1rr', 'string3b_23', 'string3.'}
 
-results_list = []
-
-for item_a in set_A:
-    max_distance = 0
-    closest_match = None
-    for item_b in set_B:
-        distance = fuzz.ratio(item_b, item_a)
-        if distance > max_distance:
-            max_distance = distance
-            closest_match = item_b
-    results_list.append([item_a, closest_match, max_distance])
-
-print('results:', results_list)
+results = find_closest_matches(set_A, set_B)
+print('results:', results)
